ab0edffa21c34bc382265688a0b3efdb300e5897
Albert Stack <albertstack@hotmail.com>
2020-11-17 13:25:25 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,13 +1,8 @@
-function firstArrivals = main(dataset,ts,f0,w,rangeWindowSize,compfa)
+function firstArrivals = main(dataset,ts,f0,w,rangeWindowSize)
 filters = ConstructFilters(ts,f0,w);
 convResults = Convolution(dataset,filters);
 rangeLine = RangeLineFinding(convResults,1);
-% rangeWindowSize = 31;
 [rangeMatrix,bias] = extractRangeMatrix(dataset,rangeLine,rangeWindowSize);
 firstArrivals = AdaptiveThreshold(rangeMatrix,f0,ts)-bias-1+rangeLine;
-acc = accuracyComparison(firstArrivals,compfa,15);
-disp(['Accuracy:',num2str(acc)]);
 figure;hold on;
-% drawFirstArrivals(dataset,rangeLine,1,'g','^',5);
 drawFirstArrivals(dataset,firstArrivals,1,'r','o',5);
-% drawFirstArrivals(dataset,compfa,0,'b','^',5);
