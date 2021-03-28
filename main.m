87c109c8801fca903337622618fa000d67978b89
Albert Stack <9657739+MrStack@users.noreply.github.com>
2021-03-28 20:36:22 +0800

    Delete main.m

--- main.m
+++
@@ -1,8 +0,0 @@
-function firstArrivals = main(dataset,ts,f0,w,rangeWindowSize)
-filters = ConstructFilters(ts,f0,w);
-convResults = Convolution(dataset,filters);
-rangeLine = RangeLineFinding(convResults,1);
-[rangeMatrix,bias] = extractRangeMatrix(dataset,rangeLine,rangeWindowSize);
-firstArrivals = AdaptiveThreshold(rangeMatrix,f0,ts)-bias-1+rangeLine;
-figure;hold on;
-drawFirstArrivals(dataset,firstArrivals,1,'r','o',5);
